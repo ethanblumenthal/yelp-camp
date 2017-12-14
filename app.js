49be517f2d0d177d5ede68ff942592e4e50feeee
Ethan Blumenthal <ethan.blumenthal@gmail.com>
2017-12-13 17:33:36 -0800

    added post functionality to add campground via form

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 
+var campgrounds = [
+    {name: 'Salmon Creek', image: 'https://www.northshorevisitor.com/wp-content/uploads/2015/05/grand-marais-campground-1.jpg'},
+    {name: 'Granite Hill', image: 'http://www.suttonfalls.com/communities/4/004/012/498/244//images/4628314067.jpg'},
+    {name: 'Mountain Goat Rest', image: 'http://blog.koa.com/wp-content/uploads/unique-campgrounds-626x417.jpg'}
+];
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 
@@ -10,16 +16,19 @@ app.get('/', function(req, res) {
 });
 
 app.get('/campgrounds', function(req, res) {
-    var campgrounds = [
-        {name: 'Salmon Creek', image: 'https://www.northshorevisitor.com/wp-content/uploads/2015/05/grand-marais-campground-1.jpg'},
-        {name: 'Granite Hill', image: 'http://www.suttonfalls.com/communities/4/004/012/498/244//images/4628314067.jpg'},
-        {name: 'Mountain Goat Rest', image: 'http://blog.koa.com/wp-content/uploads/unique-campgrounds-626x417.jpg'}
-    ];
     res.render('campgrounds', {campgrounds: campgrounds});
 });
 
-app.post('/campgrounds', function(req, res) {
+app.get('/campgrounds/new', function(req, res) {
+    res.render('new');
+});
 
+app.post('/campgrounds', function(req, res) {
+    var name = req.body.name;
+    var image = req.body.image;
+    var newCampground = {name: name, image: image};
+    campgrounds.push(newCampground);
+    res.redirect('/campgrounds');
 });
 
 app.listen(3000, function() {
