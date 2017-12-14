30fbe23518009d9c6628f6c83e2b7062e2c13358
Ethan Blumenthal <ethan.blumenthal@gmail.com>
2017-12-14 09:40:09 -0800

    added MongoDB as database for campgrounds

--- app.js
+++ app.js
@@ -1,27 +1,53 @@
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
+var express = require('express'),
+    app = express(),
+    bodyParser = require('body-parser'),
+    mongoose = require('mongoose');
 
-var campgrounds = [
-    {name: 'Salmon Creek', image: 'https://www.northshorevisitor.com/wp-content/uploads/2015/05/grand-marais-campground-1.jpg'},
-    {name: 'Granite Hill', image: 'http://www.suttonfalls.com/communities/4/004/012/498/244//images/4628314067.jpg'},
-    {name: 'Mountain Goat Rest', image: 'http://blog.koa.com/wp-content/uploads/unique-campgrounds-626x417.jpg'},    {name: 'Salmon Creek', image: 'https://www.northshorevisitor.com/wp-content/uploads/2015/05/grand-marais-campground-1.jpg'},
-    {name: 'Granite Hill', image: 'http://www.suttonfalls.com/communities/4/004/012/498/244//images/4628314067.jpg'},
-    {name: 'Mountain Goat Rest', image: 'http://blog.koa.com/wp-content/uploads/unique-campgrounds-626x417.jpg'},
-    {name: 'Salmon Creek', image: 'https://www.northshorevisitor.com/wp-content/uploads/2015/05/grand-marais-campground-1.jpg'},
-    {name: 'Granite Hill', image: 'http://www.suttonfalls.com/communities/4/004/012/498/244//images/4628314067.jpg'},
-    {name: 'Mountain Goat Rest', image: 'http://blog.koa.com/wp-content/uploads/unique-campgrounds-626x417.jpg'}
-];
-
+mongoose.connect("mongodb://localhost/yelp_camp", {useMongoClient: true});
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
+
+// Schema setup
+var campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String
+});
+
+var Campground = mongoose.model('Campground', campgroundSchema);
+
+// Campground.create({
+//     name: 'Granite Hill', image: 'http://www.suttonfalls.com/communities/4/004/012/498/244//images/4628314067.jpg'
+//     }, function(err, campground) {
+//         if (err) {
+//             console.log(err);
+//         } else {
+//             console.log(campground);
+//         }
+// });
+
+// var campgrounds = [
+//     {name: 'Salmon Creek', image: 'https://www.northshorevisitor.com/wp-content/uploads/2015/05/grand-marais-campground-1.jpg'},
+//     {name: 'Granite Hill', image: 'http://www.suttonfalls.com/communities/4/004/012/498/244//images/4628314067.jpg'},
+//     {name: 'Mountain Goat Rest', image: 'http://blog.koa.com/wp-content/uploads/unique-campgrounds-626x417.jpg'},    {name: 'Salmon Creek', image: 'https://www.northshorevisitor.com/wp-content/uploads/2015/05/grand-marais-campground-1.jpg'},
+//     {name: 'Granite Hill', image: 'http://www.suttonfalls.com/communities/4/004/012/498/244//images/4628314067.jpg'},
+//     {name: 'Mountain Goat Rest', image: 'http://blog.koa.com/wp-content/uploads/unique-campgrounds-626x417.jpg'},
+//     {name: 'Salmon Creek', image: 'https://www.northshorevisitor.com/wp-content/uploads/2015/05/grand-marais-campground-1.jpg'},
+//     {name: 'Granite Hill', image: 'http://www.suttonfalls.com/communities/4/004/012/498/244//images/4628314067.jpg'},
+//     {name: 'Mountain Goat Rest', image: 'http://blog.koa.com/wp-content/uploads/unique-campgrounds-626x417.jpg'}
+// ];
 
 app.get('/', function(req, res) {
     res.render('landing');
 });
 
 app.get('/campgrounds', function(req, res) {
-    res.render('campgrounds', {campgrounds: campgrounds});
+    Campground.find({}, function(err, allCampgrounds) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render('campgrounds', {campgrounds: allCampgrounds});
+        }
+    })
 });
 
 app.get('/campgrounds/new', function(req, res) {
