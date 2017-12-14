fd2822e447992657f2e31ef7bdff332cbc62d25c
Ethan Blumenthal <ethan.blumenthal@gmail.com>
2017-12-14 09:49:20 -0800

    added create functionality via form

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// module imports
 var express = require('express'),
     app = express(),
     bodyParser = require('body-parser'),
@@ -7,7 +8,7 @@ mongoose.connect("mongodb://localhost/yelp_camp", {useMongoClient: true});
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 
-// Schema setup
+// schema setup
 var campgroundSchema = new mongoose.Schema({
     name: String,
     image: String
@@ -15,31 +16,12 @@ var campgroundSchema = new mongoose.Schema({
 
 var Campground = mongoose.model('Campground', campgroundSchema);
 
-// Campground.create({
-//     name: 'Granite Hill', image: 'http://www.suttonfalls.com/communities/4/004/012/498/244//images/4628314067.jpg'
-//     }, function(err, campground) {
-//         if (err) {
-//             console.log(err);
-//         } else {
-//             console.log(campground);
-//         }
-// });
-
-// var campgrounds = [
-//     {name: 'Salmon Creek', image: 'https://www.northshorevisitor.com/wp-content/uploads/2015/05/grand-marais-campground-1.jpg'},
-//     {name: 'Granite Hill', image: 'http://www.suttonfalls.com/communities/4/004/012/498/244//images/4628314067.jpg'},
-//     {name: 'Mountain Goat Rest', image: 'http://blog.koa.com/wp-content/uploads/unique-campgrounds-626x417.jpg'},    {name: 'Salmon Creek', image: 'https://www.northshorevisitor.com/wp-content/uploads/2015/05/grand-marais-campground-1.jpg'},
-//     {name: 'Granite Hill', image: 'http://www.suttonfalls.com/communities/4/004/012/498/244//images/4628314067.jpg'},
-//     {name: 'Mountain Goat Rest', image: 'http://blog.koa.com/wp-content/uploads/unique-campgrounds-626x417.jpg'},
-//     {name: 'Salmon Creek', image: 'https://www.northshorevisitor.com/wp-content/uploads/2015/05/grand-marais-campground-1.jpg'},
-//     {name: 'Granite Hill', image: 'http://www.suttonfalls.com/communities/4/004/012/498/244//images/4628314067.jpg'},
-//     {name: 'Mountain Goat Rest', image: 'http://blog.koa.com/wp-content/uploads/unique-campgrounds-626x417.jpg'}
-// ];
-
+// display landing page
 app.get('/', function(req, res) {
     res.render('landing');
 });
 
+// display all campgrounds
 app.get('/campgrounds', function(req, res) {
     Campground.find({}, function(err, allCampgrounds) {
         if (err) {
@@ -50,18 +32,26 @@ app.get('/campgrounds', function(req, res) {
     })
 });
 
+// display new campground form
 app.get('/campgrounds/new', function(req, res) {
     res.render('new');
 });
 
+// add new campground to database
 app.post('/campgrounds', function(req, res) {
     var name = req.body.name;
     var image = req.body.image;
     var newCampground = {name: name, image: image};
-    campgrounds.push(newCampground);
-    res.redirect('/campgrounds');
+    Campground.create(newCampground, function(err, newlyCreated) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.redirect('/campgrounds');
+        }
+    });
 });
 
+// start server
 app.listen(3000, function() {
     console.log('Serving on port 3000');
 });
