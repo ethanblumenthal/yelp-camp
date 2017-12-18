f6821e0818be424889e325425921605727828ddd
Ethan Blumenthal <ethan.blumenthal@gmail.com>
2017-12-18 15:41:32 -0800

    associated campground data with user

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -15,20 +15,25 @@ router.get('/', function(req, res) {
 });
 
 // NEW - campground
-router.get('/new', function(req, res) {
+router.get('/new', isLoggedIn, function(req, res) {
     res.render('campgrounds/new');
 });
 
 // CREATE - campground
-router.post('/', function(req, res) {
+router.post('/', isLoggedIn, function(req, res) {
     var name = req.body.name;
     var image = req.body.image;
     var desc = req.body.description;
-    var newCampground = {name: name, image: image, description: desc};
+    var author = {
+        id: req.user._id,
+        username: req.user.username
+    }
+    var newCampground = {name: name, image: image, description: desc, author: author};
     Campground.create(newCampground, function(err, newlyCreated) {
         if (err) {
             console.log(err);
         } else {
+            console.log(newlyCreated);
             res.redirect('/campgrounds');
         }
     });
