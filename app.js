cf84d6f593e99b80f2abb0b087ab8f5b6508394f
Ethan Blumenthal <ethan.blumenthal@gmail.com>
2017-12-18 12:12:03 -0800

    added user schema

--- app.js
+++ app.js
@@ -3,9 +3,12 @@ var express = require('express'),
     app = express(),
     bodyParser = require('body-parser'),
     mongoose = require('mongoose'),
+    passport = require('passport'),
+    LocalStrategy = require('passport-local'),
     Campground = require('./models/campground'),
-    seedDB = require('./seeds'),
-    Comment = require('./models/comment');
+    Comment = require('./models/comment'),
+    User = require('./models/user'),
+    seedDB = require('./seeds');
 
 mongoose.connect("mongodb://localhost/yelp_camp", {useMongoClient: true});
 mongoose.Promise = global.Promise;
