26034f26c99372f5a49beb227669487f95cf8450
Ethan Blumenthal <ethan.blumenthal@gmail.com>
2017-12-18 13:31:22 -0800

    added login functionality

--- app.js
+++ app.js
@@ -127,6 +127,18 @@ app.post('/signup', function(req, res) {
     });
 });
 
+// SHOW - login
+app.get('/login', function(req, res) {
+    res.render('login');
+});
+
+// CREATE - login
+app.post('/login', passport.authenticate('local', {
+    successRedirect: 'campgrounds',
+    failureRedirect: 'login'
+}), function(req, res) {
+});
+
 // start server
 app.listen(3000, function() {
     console.log('Serving on port 3000');
