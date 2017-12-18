4137a87a87982fec096942c9ba6298020fec7387
Ethan Blumenthal <ethan.blumenthal@gmail.com>
2017-12-18 14:00:07 -0800

    added current user middleware

--- app.js
+++ app.js
@@ -29,6 +29,12 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+// current user middleware
+app.use(function(req, res, next) {
+    res.locals.currentUser = req.user;
+    next();
+});
+
 // render landing page
 app.get('/', function(req, res) {
     res.render('landing');
